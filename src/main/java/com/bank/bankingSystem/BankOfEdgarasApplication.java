56691e4a0d8c65678a69ffd57ed4c6af323c17f1
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2020-09-12 11:03:09 +0200

    Initial commit

---
+++ src/main/java/com/bank/bankingSystem/BankOfEdgarasApplication.java
@@ -0,0 +1,13 @@
+package com.bank.bankingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfEdgarasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankOfEdgarasApplication.class, args);
+	}
+
+}
